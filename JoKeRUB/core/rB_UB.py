79e95e66e0ed21fde8cc46ab212006a9ce9bbbda
aliadai <166298469+aliadai@users.noreply.github.com>
2025-08-05 16:34:00 +0300

    Update rB_UB.py

--- JoKeRUB/core/rB_UB.py
+++ JoKeRUB/core/rB_UB.py
@@ -1,3 +1,5 @@
 def rB_Pyro(string_session, logs):
+    if not isinstance(string_session, str):
+        raise ValueError("The given session must be a str or a Session instance.")
     print(f"Session created with: {string_session} and logs: {logs}")
     return {"session": string_session, "logs": logs}
