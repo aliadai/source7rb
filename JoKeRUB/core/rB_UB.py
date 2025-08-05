c0c4cf2393b83b9e88a78aa6495db66002dcec8e
aliadai <166298469+aliadai@users.noreply.github.com>
2025-08-05 16:42:10 +0300

    Update rB_UB.py

--- JoKeRUB/core/rB_UB.py
+++ JoKeRUB/core/rB_UB.py
@@ -1,13 +1,13 @@
 from telethon import TelegramClient
 from telethon.sessions import StringSession
-from .. import Config  # أو عدّل الاستيراد حسب مكان ملف Config
+from JoKeRUB.exampleconfig import APP_ID, APP_HASH, STRING_SESSION  # عدل المسار إذا لزم
 
 def rB_Pyro(string_session, logs):
     if not isinstance(string_session, str):
         raise ValueError("The given session must be a str or a Session instance.")
     client = TelegramClient(
         StringSession(string_session),
-        api_id=Config.API_ID,
-        api_hash=Config.API_HASH
+        api_id=APP_ID,
+        api_hash=APP_HASH
     )
     return client
