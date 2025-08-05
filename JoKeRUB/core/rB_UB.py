024ca5f61e57973f3d234b74c40bd964105ffbd7
aliadai <166298469+aliadai@users.noreply.github.com>
2025-08-05 16:34:38 +0300

    Update rB_UB.py

--- JoKeRUB/core/rB_UB.py
+++ JoKeRUB/core/rB_UB.py
@@ -1,5 +1,7 @@
+from pyrogram import Client
+
 def rB_Pyro(string_session, logs):
     if not isinstance(string_session, str):
         raise ValueError("The given session must be a str or a Session instance.")
-    print(f"Session created with: {string_session} and logs: {logs}")
-    return {"session": string_session, "logs": logs}
+    app = Client(string_session, logger=logs)
+    return app
