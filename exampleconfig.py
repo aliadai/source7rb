069c5cb4cbac3481fb9c9731026d71c6cf53e1ee
aliadai <166298469+aliadai@users.noreply.github.com>
2025-08-20 13:25:16 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -4,13 +4,13 @@ class Development(Config):
     APP_ID = "26044200"
     API_HASH = "a0e21a02333bcff6b9e007600632adeb"
     # the name to display in your alive message
-    ALIVE_NAME = "ذوالفقار"
+    ALIVE_NAME = "."
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
     DB_URI = "postgresql://joker510:joker510@localhost:5432/joker510"
     # After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
-    STRING_SESSION = "1ApWapzMAUC6LYm2E2MeyjMjlUv3VCX1X9o8q1pAAHY-yfm4eEdBpgT_EUsO9tVIaKfaZNsQfXRYfhRsgGLDTEHPNRl-PfOPQrHqNA5eby-YOb4qVEkLdiukGovyUj8YqEIN_Hct8zzmsA8WXJNRAAJ5uDeh1E4ENrtmLu6ul-dtvEdTNo7rH8pKE1wCMAZasVuf7Lj9fnnhh94qOpywR5NvybYV0eG2rhGvc1iMt6PqF3AxC1p8sRYUxKlTv_c36NTI4MvY0COeiVW9HV5HVuswEj-viaE_cT3nyf6_LSqeXwzKOdETvQCMhk7LqamIxPVNQAnqQkxNCBmIE73wXG8DfzFspvPI="
+    STRING_SESSION = "تيرمكس"
     # create a new bot in @botfather and fill the following vales with bottoken and username respectively
-    TG_BOT_TOKEN = "6661204565:AAE-RWZHCnEQ-bVoxtEP_rhjnFC0BwFNRVk"
+    TG_BOT_TOKEN = "توكن"
     # command handler
     COMMAND_HAND_LER = "."
     # sudo enter the id of sudo users userid's in that array
