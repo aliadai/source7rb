d523289055e8dc1b37a39b871f8e609f11f406f1
aliadai <166298469+aliadai@users.noreply.github.com>
2025-12-20 21:35:13 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -3,17 +3,14 @@ class Development(Config):
     # get this values from the my.telegram.org
     APP_ID = "26044200"
     API_HASH = "a0e21a02333bcff6b9e007600632adeb"
-    # the name to display in your alive message
     ALIVE_NAME = "."
-    # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
     DB_URI = "postgresql://joker510:joker510@localhost:5432/joker510"
-    # After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
     STRING_SESSION = "تيرمكس"
-    # create a new bot in @botfather and fill the following vales with bottoken and username respectively
     TG_BOT_TOKEN = "توكن"
-    # command handler
     COMMAND_HAND_LER = "."
-    # sudo enter the id of sudo users userid's in that array
     SUDO_USERS = []
-    # command hanler for sudo
     SUDO_COMMAND_HAND_LER = "."
+
+    # هنا تضيف بيانات GitHub
+    GITHUB_USERNAME = "aliadai"
+    GITHUB_TOKEN = "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"
