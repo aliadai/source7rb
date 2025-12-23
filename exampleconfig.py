c9bb2502c1d1f475af3fadb10038cdd86a7f5a0d
aliadai <166298469+aliadai@users.noreply.github.com>
2025-12-23 11:17:18 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -1,6 +1,6 @@
 from sample_config import Config
+
 class Development(Config):
-    # get this values from the my.telegram.org
     APP_ID = "26044200"
     API_HASH = "a0e21a02333bcff6b9e007600632adeb"
     ALIVE_NAME = "."
@@ -10,7 +10,5 @@ class Development(Config):
     COMMAND_HAND_LER = "."
     SUDO_USERS = []
     SUDO_COMMAND_HAND_LER = "."
-
-    # هنا تضيف بيانات GitHub
     GITHUB_USERNAME = "aliadai"
-    GITHUB_TOKEN = "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"
+    GITHUB_TOKEN = ""
